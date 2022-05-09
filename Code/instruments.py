2307590f2d037210dbef013651a41a7a59c61f51
ismael <ismaelmf@gmx.de>
2022-05-09 15:38:38 +0200

    saving 2 instruments

---
+++ Code/instruments.py
@@ -0,0 +1,39 @@
+# here functions will be defined
+# the functions will pick up some data from the test subset
+# and make a prediction on them
+
+def load_model(model_name):
+    model = keras.models.load_model(model_name)
+    return model
+
+def load_var()
+
+def function_1:
+    """pick one element of the train subset
+    make a prediction
+    see if it work"""
+
+
+
+
+def function_2:
+    """pick a given number of elements
+    of a given number of instruments
+    make predictions and see if they worked
+    """
+
+
+def function_3:
+
+
+
+if __name__=="__main__":
+
+    # here the input will be read
+    # input about which test data to pick
+    # and how to pickt it
+
+    # input=''
+    model_name
+    if input == 1:
+        model = load_model(model_name)
